61cbdbaf513011c5ca310aff7be73878f4feaac2
eatMelon-Masses <z11764404@gmail.com>
2022-09-28 00:29:13 +0800

    refactor: client ip接口

--- yz-tc-adapter/src/main/java/yiezi/ml/web/InstanceController.java
+++ yz-tc-adapter/src/main/java/yiezi/ml/web/InstanceController.java
@@ -1,18 +1,22 @@
 package yiezi.ml.web;
 
 import com.alibaba.cola.dto.MultiResponse;
+import com.alibaba.cola.dto.SingleResponse;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import yiezi.ml.api.InstanceServiceI;
 import yiezi.ml.dto.data.TxInstanceDTO;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 
 /**
  * @author zhouye
  * @date 2022/9/12
  */
 @RestController
+@Slf4j
 public class InstanceController {
     @Resource
     private InstanceServiceI instanceServiceI;
@@ -20,5 +24,16 @@ public class InstanceController {
     @GetMapping(value = "/instances")
     public MultiResponse<TxInstanceDTO> listInstances() {
         return instanceServiceI.getInstanceAll();
+    }
+
+    @GetMapping(value = "/client/ip")
+    public SingleResponse<String> getClientIp(HttpServletRequest request) {
+        String ipAddress = null;
+        ipAddress = request.getHeader("X-FORWARDED-FOR");   // proxy
+        if (ipAddress == null) {
+            ipAddress = request.getRemoteAddr();
+        }
+        log.info("IP Address: " + ipAddress);
+        return SingleResponse.of(ipAddress);
     }
 }
